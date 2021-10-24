c92f5a91bd8ab6eef5ecdbbdac50e73d3058294d
Sebastian Zielke <harry48@arcor.de>
2021-10-24 23:41:13 +0200

    prepared for vercel deployment

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const ejs = require('ejs');
 const path = require('path');
 const UserService = require('./userService');
+const product = require('./api/product');
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const { v4 } = require('uuid');
 
 const INPUT = path.join(__dirname, 'index.ejs')
 
 const service = new UserService();
 
+app.get('/api2', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
+  
+  app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+  });
+    
 app.get('/', (request, response) => {
     ejs.renderFile(
         INPUT,
@@ -27,6 +42,10 @@ app.get('/', (request, response) => {
     );
 });
 
+app.use(express.json({extended: false}));
+
+app.use("/api/product", product)
+
 app.listen(PORT, (error) => {
     if (error) {
         console.error(error);
@@ -34,3 +53,5 @@ app.listen(PORT, (error) => {
         console.log(`Server started at: http://localhost:${PORT}`);
     }
 });
+
+module.exports = app;
